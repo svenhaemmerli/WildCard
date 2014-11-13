efc71887e78f6561f2c031aa8492c602a155841d
Sven <Sven@10.207.6.236>
2014-11-13 13:10:10 +0100

    GUI update

--- WildCard/src/ch/fhnw/haggis/gui/GuiLoginScreen.java
+++ WildCard/src/ch/fhnw/haggis/gui/GuiLoginScreen.java
@@ -15,42 +15,57 @@ import java.awt.event.ActionListener;
 
 
 
+@SuppressWarnings("serial")
 public class GuiLoginScreen extends JFrame implements ActionListener{
 	
-	JLabel username = new JLabel();
-	JTextArea eingabe = new JTextArea();
+	//JLabel username = new JLabel();
+	JTextField username;
 	JLabel lblBackground;
+	
+	private JPanel panelSouth;
+	private JPanel panelSouthCenter;
+	private JPanel panelSouthSouth;
+	private JButton btnLogin;
 
 	public GuiLoginScreen (){
 		setTitle("Haggis - WILDCARD");
 		setSize(500, 500);
 		setLocationRelativeTo(null); //place it in the center of the screen
-		setResizable(true); 
+		setResizable(false); 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		getContentPane().setLayout(new BorderLayout());
+		
 		((JPanel)getContentPane()).setOpaque(false);
 		ImageIcon background = new ImageIcon(getClass().getResource("img/0101_wildcard_logo.png"));
 		lblBackground = new JLabel(background);
 		getLayeredPane().add(lblBackground, new Integer(Integer.MIN_VALUE));
 		lblBackground.setBounds(25, 30, background.getIconWidth(), background.getIconHeight());
 		
+		//panel south create
+		panelSouth = new JPanel();
+		panelSouth.setLayout(new BorderLayout());
+		panelSouth.setSize(new Dimension(300,500));
+		getContentPane().add(panelSouth, BorderLayout.SOUTH); //put panel south into the south of the content pane
 		
+		panelSouthCenter = new JPanel(); //create panel southCenter
+		panelSouth.add(panelSouthCenter, BorderLayout.CENTER); //add the panelSouthCenter to the center of panelSouth
+		
+		//create a JTextfield
+		username = new JTextField("Username",10); //set title and size of the jtextfield possibly a test afterwards with Username max. 16 characters
 		username.setOpaque(false);
 		username.setForeground(Color.black);
-		username.setText("Username");
-		username.setBackground(Color.white);
 		username.setHorizontalAlignment(SwingConstants.CENTER);
-		getContentPane().add(username, BorderLayout.CENTER);
+		panelSouthCenter.add(username);
 		
-		/*
-		eingabe.setOpaque(false);
-		eingabe.setForeground(Color.BLACK);
-		eingabe.setBackground(Color.white);
 		
-		this.add(eingabe);
-		*/
+		panelSouthSouth = new JPanel();
+		panelSouth.add(panelSouthSouth, BorderLayout.SOUTH);
 		
+		btnLogin = new JButton("login");
+		btnLogin.addActionListener(this);
+		panelSouthSouth.add(btnLogin);
+		//make window visible
 		setVisible(true);
 	}
 	
@@ -59,22 +74,20 @@ public class GuiLoginScreen extends JFrame implements ActionListener{
 	public static void main(String[] args){
 		GuiLoginScreen gui = new GuiLoginScreen();
 		}
+	//Method to get the content of username
+	String getUserInput() {
+		return username.getText();
+		}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	//the input is set through the user
+	void setUserInput(String string){
+		username.setText(string);
+		}
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent ae) {
+		//String text = text.getUserInput();
+		//System.out.println(ae);
 		
 	}
 
