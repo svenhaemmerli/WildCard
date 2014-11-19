e176ae5411d28f9c6fcbb333733db9e623d64fc6
mäddy <mddy@mddy-HP>
2014-11-19 13:15:59 +0100

    New Client Server

--- WildCard/src/ch/fhnw/haggis/client/Client.java
+++ WildCard/src/ch/fhnw/haggis/client/Client.java
@@ -9,31 +9,57 @@ import java.net.*;
 public class Client {
 	
 	private Socket cSocket;
-	//public ObjectInputStream input;
-	//public ObjectOutputStream output;
+	public ObjectInputStream input;
+	public ObjectOutputStream output;
 	public String address;
+	public Object rob;
+	
 
 	// Make connection to server
 	public void startClient() {
 		try {
-		address = Inet4Address.getLocalHost().getHostAddress();
-						
+		//get Local Host address 	
+		address = Inet4Address.getLocalHost().getHostAddress(); 
+		
+			//Inizialise socket and connect to ServerSocket
 			cSocket = new Socket("localhost", 5000);
 			System.out.println("Client gestartet");
 			
-			//input = new ObjectInputStream(cSocket.getInputStream());
+			//Instanciate Object input and output stream
 			
-			//output = new ObjectOutputStream(cSocket.getOutputStream());
+			input = new ObjectInputStream(cSocket.getInputStream());
+					
+			output = new ObjectOutputStream(cSocket.getOutputStream());
 		} 
 		catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		try {
+			rob = input.readObject();
+			if (rob!= null){
+							
+			System.out.println("Listenelement: "+ rob);
+		}
+		
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 		
 		public static void main(String args[]) {
-		Client c = new Client ();
-		c.startClient();
 		
+			Client c = new Client ();
+			c.startClient();
+			
+				
+			
 		
 	}
 
--- WildCard/src/ch/fhnw/haggis/server/Server.java
+++ WildCard/src/ch/fhnw/haggis/server/Server.java
@@ -3,13 +3,19 @@ package ch.fhnw.haggis.server;
 import java.net.*;
 import java.io.*;
 
+import ch.fhnw.haggis.client.Client;
+
 public class Server {
 
 	private Player players[];
 	private ServerSocket sSocket;
 	public String Localhost;
 	public Socket cSocket;
-
+	public ObjectInputStream input;
+	public ObjectOutputStream output;
+	
+	
+	
 	public Server() {
 
 		players = new Player[3];
@@ -21,23 +27,28 @@ public class Server {
 			
 			cSocket = sSocket.accept();
 			
-		} catch (IOException e) {
+			//Instanciate Object input and output stream
+			
+			input = new ObjectInputStream(cSocket.getInputStream());
+					
+			output = new ObjectOutputStream(cSocket.getOutputStream());
+			
+			Card ob = new Card ("joker_queen1",3,12,"joker");
+			output.writeObject(ob);
+			output.flush();
+			System.out.println("Objekt gesendet");
+			
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 			System.exit(1);
-		}
-		
-		try {
-			Localhost = Inet4Address.getLocalHost().getHostAddress();
-			System.out.println(Localhost);
-			
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
 		}
 
 	}
 
 	public static void main(String args[]) {
 		Server game = new Server();
+	
 	}
 }
