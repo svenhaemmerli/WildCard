b97c58abaf8d45edecf896552ade2e69f8c28c30
Ivory55 <ivo.hausammann@gmail.com>
2014-11-26 13:42:32 +0100

    add class playerWildcard

--- WildCard/src/ch/fhnw/haggis/server/Card.java
+++ WildCard/src/ch/fhnw/haggis/server/Card.java
@@ -62,7 +62,7 @@ public class Card {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Hallo");
+		System.out.println();
 		
 		
 	}
---
+++ WildCard/src/ch/fhnw/haggis/server/playerWildcard.java
@@ -0,0 +1,13 @@
+package ch.fhnw.haggis.server;
+
+public class playerWildcard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
